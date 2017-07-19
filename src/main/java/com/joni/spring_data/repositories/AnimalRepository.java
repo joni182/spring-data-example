8397eeee4f3dc03037bdb8c69b033e398f8c7baa
Fernando Llaca <ferllarom@gmail.com>
2017-07-19 20:34:20 +0200

    Modificada la configuración de Spring Data
    Ahora leemos la configuración a partir de un archivo (application.yml). Tenemos que añadir la anotación @Repository a aquellas clases que sean "repositorios" JPA.

--- src/main/java/com/joni/spring_data/repositories/AnimalRepository.java
+++ src/main/java/com/joni/spring_data/repositories/AnimalRepository.java
@@ -3,10 +3,12 @@ package com.joni.spring_data.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.joni.spring_data.entities.Animals;
 import com.joni.spring_data.entities.Animals;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animals,String> {
 
 	Animals findOne(String arg0);
